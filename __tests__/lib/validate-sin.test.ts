09ebdd87367d71bfdfb44fb65dfa5c1821f9215e
Mehrdad Youssefi <themehrdad@gmail.com>
2024-10-09 02:27:40 -0700

    validate-sin and test ready

---
+++ __tests__/lib/validate-sin.test.ts
@@ -0,0 +1,40 @@
+import "@testing-library/jest-dom";
+import { describe, expect, it } from "@jest/globals";
+import { validateSin } from "@/lib/validate-sin";
+
+describe("validate-sin", () => {
+  it("should fail if the sin has less than 9 characters", () => {
+    const sin = "123";
+    const result = validateSin(sin);
+    expect(result.valid).toBe(false);
+    expect(result.reason).toBe("SIN must be 9 digits long");
+  });
+
+  it("should fail if the sin has more than 9 characters", () => {
+    const sin = "1234567890";
+    const result = validateSin(sin);
+    expect(result.valid).toBe(false);
+    expect(result.reason).toBe("SIN must be 9 digits long");
+  });
+
+  it("should fail if the sin contains non-digits", () => {
+    const sin = "12345678a";
+    const result = validateSin(sin);
+    expect(result.valid).toBe(false);
+    expect(result.reason).toBe("SIN must contain only digits");
+  });
+
+  it("should fail if the sin checksum check fails", () => {
+    const sin = "123456789";
+    const result = validateSin(sin);
+    expect(result.valid).toBe(false);
+    expect(result.reason).toBe("Invalid SIN");
+  });
+
+  it("should pass if the sin is valid", () => {
+    const sin = "046454286";
+    const result = validateSin(sin);
+    expect(result.valid).toBe(true);
+    expect(result.reason).toBe("");
+  });
+});
---
+++ src/lib/validate-sin.ts
@@ -0,0 +1,45 @@
+export function validateSin(sin: string) {
+  if (!validateLength(sin)) {
+    return { valid: false, reason: "SIN must be 9 digits long" };
+  }
+
+  if (!validateDigits(sin)) {
+    return { valid: false, reason: "SIN must contain only digits" };
+  }
+
+  if (!validateLuhn(sin)) {
+    return { valid: false, reason: "Invalid SIN" };
+  }
+
+  return { valid: true, reason: "" };
+}
+
+function validateLength(sin: string) {
+  return sin.length === 9;
+}
+
+function validateDigits(sin: string) {
+  return sin.match(/^\d+$/) !== null;
+}
+
+function validateLuhn(sin: string) {
+  const digits = sin.split("").map(Number);
+  const secondDigitsDoubled = digits.map((d, i) => {
+    if (i % 2 === 0) {
+      return d;
+    }
+    const doubled = d * 2;
+    if (doubled > 9) {
+      return doubled - 9;
+    }
+    return doubled;
+  });
+
+  const sum = secondDigitsDoubled.reduce((acc, d) => acc + d, 0);
+  return sum % 10 === 0;
+}
+
+type ValidateSinResult = {
+  valid: boolean;
+  reason: string;
+};
